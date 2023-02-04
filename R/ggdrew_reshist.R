ebdb9b1c507159fed6bbd26e816383206bc7dd8f
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2023-02-04 03:33:03 -0800

    Update ggdrew_reshist.R
    * Added the ability to select # of bins
    * Added color for borders of histogram

--- R/ggdrew_reshist.R
+++ R/ggdrew_reshist.R
@@ -1,10 +1,18 @@
-ggdrew_reshist <- function(model) {
-  residuals(model) %>%
-    as.data.frame() |>
-    ggplot(aes(x = .)) +
-    geom_histogram(bins = 30, fill = "#333131") +
-    labs(title = 'Histogram of Residuals',
-         x = 'Frequency',
-         y = 'Residuals') +
-    theme(plot.title = element_text(hjust = 0.5))
+ggdrew_reshist <- function(model, bins = NULL) {
+  if (is.null(bins)) {
+    return (residuals(model) %>%
+              as.data.frame() |>
+              ggplot(aes(x = .)) +
+                geom_histogram(bins = 30, color = "#595757", fill = "#333131") +
+                labs(title = 'Histogram of Residuals', x = 'Frequency', y = 'Residuals') +
+                theme(plot.title = element_text(hjust = 0.5)))
+  }
+  else {
+    return (residuals(model) %>%
+              as.data.frame() |>
+              ggplot(aes(x = .)) +
+                geom_histogram(bins = bins, color = "#595757", fill = "#333131") +
+                labs(title = 'Histogram of Residuals', x = 'Frequency', y = 'Residuals') +
+                theme(plot.title = element_text(hjust = 0.5)))
+  }
 }
