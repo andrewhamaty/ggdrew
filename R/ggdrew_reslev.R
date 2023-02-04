b8f007d50a36e2371dcafd1c99f3b59790bcb2a7
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2023-02-04 01:49:43 -0800

    Update ggdrew_reslev.R
    Prettified syntax

--- R/ggdrew_reslev.R
+++ R/ggdrew_reslev.R
@@ -2,10 +2,10 @@ ggdrew_reslev <- function(model) {
   cbind(rstandard(model), hatvalues(model)) |>
     as.data.frame() |>
     ggplot(aes(x = V2, y = V1)) +
-    geom_point() +
-    geom_smooth(formula = y ~ x, method = "loess", se = FALSE, color = "red") +
-    labs(title = 'Residual vs. Leverage',
-         x = 'Leverage',
-         y = 'Residuals') +
-    theme(plot.title = element_text(hjust = 0.5))
+      geom_point() +
+      geom_smooth(formula = y ~ x, method = "loess", se = FALSE, color = "red") +
+      labs(title = 'Residual vs. Leverage',
+           x = 'Leverage',
+           y = 'Residuals') +
+      theme(plot.title = element_text(hjust = 0.5))
 }
