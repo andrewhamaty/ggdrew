91849292aec74a0e30aa590c209f797212bc3b11
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2023-02-04 23:21:39 -0800

    Rewrite + Cook's Distance
    Simplified obtaining the residuals and added an option to plot with Cook's Distance

--- R/ggdrew_reslev.R
+++ R/ggdrew_reslev.R
@@ -1,11 +1,21 @@
-ggdrew_reslev <- function(model) {
-  cbind(rstandard(model), hatvalues(model)) |>
-    as.data.frame() |>
-    ggplot(aes(x = V2, y = V1)) +
-      geom_point() +
-      geom_smooth(formula = y ~ x, method = "loess", se = FALSE, color = "red") +
-      labs(title = 'Residual vs. Leverage',
-           x = 'Leverage',
-           y = 'Residuals') +
-      theme(plot.title = element_text(hjust = 0.5))
+ggdrew_reslev <- function(model, cooksd = FALSE) {
+  ggplot(model, aes(.hat, .stdresid)) +
+    geom_hline(linewidth = 2,
+               linetype = "dashed",
+               color = "black",
+               yintercept = 0,
+               alpha = 0.3) +
+    geom_smooth(color = "red",
+                se = FALSE,
+                formula = "y ~ x",
+                method = "loess") +
+    labs(title = 'Residuals vs. Leverage',
+         x = 'Leverage',
+         y = 'Residuals') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    (if (cooksd) {
+      geom_point(aes(size = .cooksd))
+    } else {
+      geom_point()
+    })
 }
