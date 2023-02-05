1ee1da79218878431d39dc2ec08151c873e6c814
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2023-02-04 21:38:10 -0800

    Update ggdrew_cormatrix.R
    Added an option to create a correlation matrix with circles instead of squares

--- R/ggdrew_cormatrix.R
+++ R/ggdrew_cormatrix.R
@@ -1,6 +1,14 @@
-ggdrew_cormatrix <- function(data) {
-  data |>
-    select_if(is.numeric) |>
-    cor() |>
-    ggcorrplot()
+ggdrew_cormatrix <- function(data, method = NULL) {
+  if (!is.null(method) && method == "circle") {
+    return (data %>%
+              select_if(is.numeric) %>%
+              cor() %>%
+              ggcorrplot(method = "circle"))
+  }
+  else {
+    return (data %>%
+              select_if(is.numeric) %>%
+              cor() %>%
+              ggcorrplot())
+  }
 }
