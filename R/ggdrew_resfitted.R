ebda6a9e180879ed691c8d0b9893b421391d8628
andrewhamaty <113324413+andrewhamaty@users.noreply.github.com>
2023-02-07 17:40:47 -0800

    fixed problem with geom_smooth
    changed the formula for geom_smooth

--- R/ggdrew_resfitted.R
+++ R/ggdrew_resfitted.R
@@ -2,10 +2,14 @@ ggdrew_resfitted <- function(model) {
   ggplot(model, aes(x = .fitted, y = .resid)) +
     geom_point() +
     geom_smooth(formula = y ~ x,
-                method = lm,
-                color = "red",
-                linetype = "longdash",
+                method = loess,
+                color = "blue",
+                linetype = "solid",
                 alpha = 0.35) +
+    geom_hline(yintercept = 0,
+               color = "red",
+               size = 0.7,
+               linetype = 5) +
     labs(title = 'Residuals vs. Fitted Values',
          x = 'Fitted Values',
          y = 'Residuals') +
